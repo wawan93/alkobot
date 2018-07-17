1c7bf635305afa29619af95c002062283dcb8184
vkochergin <vvk@rdw.ru>
2018-07-17 19:35:04 +0300

    tg bot api

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/wawan93/bot-framework"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	token := os.Getenv("TOKEN")
+	fmt.Printf("%v\n", token)
+	api, _ := tgbotapi.NewBotAPI(token)
+
+	u := tgbotapi.NewUpdate(0)
+	updates, _ := api.GetUpdatesChan(u)
+
+	bot := tgbot.NewBotFramework(api)
+
+	bot.RegisterCommand("/start", func(bot *tgbot.BotFramework, update *tgbotapi.Update) error {
+		chatID := bot.GetChatID(update)
+		msg := tgbotapi.NewMessage(chatID, "Hello, World!")
+		_, err := bot.Send(msg)
+		return err
+	}, 0)
+
+	bot.HandleUpdates(updates)
 }
